ac0e67d72b48ccfff2c9d635b6e585ac56233e27
william <william.del.rosario@iceshop.nl>
2020-02-23 19:45:56 +0100

    gotchas around providers

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ class Button extends React.Component {
 
     render() {
         const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-        console.log(this.context)
+
         return <button className="ui button primary">{text}</button>
     }
 
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,8 @@
 import React from "react";
 
-export default React.createContext('english')
+export default React.createContext('english');
+
+// const context = React.createContext('english');
+// console.log(context)
+//
+// export default context;
