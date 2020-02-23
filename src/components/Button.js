508a31a8f044c38f6c3ed53706c04e0a9c7f64c4
william <william.del.rosario@iceshop.nl>
2020-02-23 19:30:20 +0100

    consuming the context value

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,8 +5,9 @@ class Button extends React.Component {
     static contextType = LanguageContext;
 
     render() {
+        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
         console.log(this.context)
-        return <button className="ui button primary">Submit</button>
+        return <button className="ui button primary">{text}</button>
     }
 
 }
