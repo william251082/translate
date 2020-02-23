b50d8eeae4f52c15dea570b188e34d1098db5eaa
william <william.del.rosario@iceshop.nl>
2020-02-23 20:52:03 +0100

    pulling from multiple context

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,8 +1,3 @@
 import React from "react";
 
 export default React.createContext('english');
-
-// const context = React.createContext('english');
-// console.log(context)
-//
-// export default context;
