23ccf3487342a3c1a8cb9eb194f79e7fe313dc27
william <william.del.rosario@iceshop.nl>
2020-03-01 11:47:43 +0100

    lifeCycles with useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,29 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
-class ResourceList extends React.Component {
-    state = { resources: [] };
+const ResourceList = ({resource}) => {
+    const [resources, setResources] = useState([]);
 
-    async componentDidMount() {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+    const fetchResource = async (resource) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
-        this.setState({ resources: response.data })
-    }
+        setResources(response.data)
+    };
 
-    // gets called everytime a component gets updated
-    // don't just copy componentDidMount without prevProps
-    async componentDidUpdate(prevProps) {
-        console.log(prevProps);
-        if (prevProps.resource !== this.props.resource) {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+    useEffect(() => {
+        fetchResource(resource);
+    }, []);
 
-            this.setState({ resources: response.data })
-        }
-    }
-
-    render() {
-        return <div>{this.state.resources.length}</div>
-    }
-}
+    return <div>{resources.length}</div>
+};
 
 export default ResourceList;
