6e83b5746a14b76caf7589efad77e14734907c87
william <william.del.rosario@iceshop.nl>
2020-03-01 11:26:28 +0100

    componentDidMount vs componentDidUpdate

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -10,6 +10,17 @@ class ResourceList extends React.Component {
         this.setState({ resources: response.data })
     }
 
+    // gets called everytime a component gets updated
+    // don't just copy componentDidMount without prevProps
+    async componentDidUpdate(prevProps) {
+        console.log(prevProps);
+        if (prevProps.resource !== this.props.resource) {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+            this.setState({ resources: response.data })
+        }
+    }
+
     render() {
         return <div>{this.state.resources.length}</div>
     }
